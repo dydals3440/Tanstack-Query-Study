de56c5942c89d191325aef52b63c78399e55befe
김용민 <dydals3440@gmail.com>
2024-04-07 21:38:34 +0900

    feat: 쿼리 구성 객체 및 요청 취소

--- src/util/http.js
+++ src/util/http.js
@@ -1,11 +1,13 @@
-export async function fetchEvents(searchTerm) {
+// {searchTerm, signal} 이렇게 destructuring가능
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm);
   let url = 'http://localhost:3000/events';
 
   if (searchTerm) {
     url += '?search=' + searchTerm;
   }
-
-  const response = await fetch(url);
+  // 브라우저는 이 취소 신호를 받아 중지함.
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
