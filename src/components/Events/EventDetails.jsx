dcb60a9209b059f4c2007eba077b7bd302582fc9
김용민 <dydals3440@gmail.com>
2024-04-08 15:47:42 +0900

    feat: 삭제 모달 버튼 추가, 로딩 에러메시지 구현

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -2,10 +2,14 @@ import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
+import Modal from '../UI/Modal.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
+import { useState } from 'react';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const { id } = useParams();
   const navigate = useNavigate();
 
@@ -14,15 +18,31 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
+        // invalidateQueries가 호출될떄, 즉시 자동으로 다시 트리거 되지 않음. 대신 기존 쿼리는 무효화되고, 다음번에 요청될떄 다시 실행됩니다. 하지만 즉시 다시 트리거되지않음!
         queryKey: ['events'],
+        // 기존 쿼리는 무효화되고, 다음번에 요청될 떄 다시 실행 (처음에 무효화시키면 삭제되자마자, 한번더 요청을해서 없는 리소스에 한번더 접근하게 됨 -> 요청 낭비)
+        refetchType: 'none',
       });
       navigate('/events');
     },
   });
+
+  const handleStartDelete = () => {
+    setIsDeleting(true);
+  };
+
+  const handleStopDelete = () => {
+    setIsDeleting(false);
+  };
 
   const handleDelete = () => {
     mutate({ id });
@@ -64,7 +84,7 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={handleStartDelete}>Delete</button>
             <Link to='edit'>Edit</Link>
           </nav>
         </header>
@@ -85,6 +105,37 @@ export default function EventDetails() {
   }
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this event? This action cannot be
+            undone
+          </p>
+          <div className='form-actions'>
+            {isPendingDeletion && <p>Deleting, please wait...</p>}
+            {!isPendingDeletion && (
+              <>
+                <button onClick={handleStopDelete} className='button-text'>
+                  Cancel
+                </button>
+                <button onClick={handleDelete} className='button'>
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          {isErrorDeleting && (
+            <ErrorBlock
+              title='Failed to delete Event'
+              message={
+                deleteError.info?.message ||
+                'Failed to delete event, please try again later'
+              }
+            />
+          )}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to='/events' className='nav-item'>
