46789ac07e2a5ce0a11f0f91945ac0603fccd50b
김용민 <dydals3440@gmail.com>
2024-04-08 11:21:42 +0900

    feat: 변형 성공시 동작 및 쿼리 무효화

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -5,16 +5,26 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
   // isError일떄는 에러일때, error는 에러메시지가 담김
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      // exact가 true면 정확히 일치하는 쿼리 ['events', { searchTerm }] 이렇게만된다.
+      // {queryKey: ['events'], exact: true]}
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    },
+    onError: () => {},
   });
 
   function handleSubmit(formData) {
     mutate({ event: formData });
+    // mutation 성공과 실패에 상관없이 events 페이지로 가게됨. (onSuccess, onError로 핸들링하는게 직관적)
+    // navigate('/events');
   }
 
   return (
--- src/util/http.js
+++ src/util/http.js
@@ -1,3 +1,7 @@
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
+
 // {searchTerm, signal} 이렇게 destructuring가능
 export async function fetchEvents({ signal, searchTerm }) {
   console.log(searchTerm);
